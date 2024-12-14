85984774dfb26a1dd52939117a7d306436167a45
Hammad Raza <01hammadraza@gmail.com>
2024-12-15 04:51:00 +0500

    'addinginpsg'

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,34 @@
 import express from "express";
-import nodemon from "nodemon";
-import mongoose from "mongoose";
-import cors from "cors"
+import cors from "cors";
 import { createForm, getForm } from "./controllers/FormControllers.js";
 import { login, signUp } from "./controllers/UserController.js";
-const app=express()
-app.use(express.json())
-app.use(cors())
+import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv";
 
-mongoose.connect("mongodb://localhost:27017",{
-    dbName:"surveyForm"
-}).then(()=>{
-    console.log("db connected")
-}).catch(()=>{
-    console.log("connection refused")
-})
+dotenv.config(); 
+const sql = neon(process.env.DATABASE_URL); 
+const app = express();
 
-app.get("/",getForm)
-app.post("/create",createForm)
-
-app.post("/login",login)
-app.post("/signup",signUp)
+app.use(express.json());
+app.use(cors());
 
 
-app.listen(8080 ,()=>{
-    console.log("server started ")
-})
+const conn=async ()=>{
+    try {
+        await sql `SELECT 1`;
+        console.log("Database Connected")
+        app.listen(8080,()=>{
+            console.log("server started ")
+        })
+    } catch (error) {
+        console.error("failed to connect to the db",error.messsage);
+        process.exit(1);
+    }
+}
+
+conn()
+// Routes
+app.get("/", getForm);
+app.post("/create", createForm);
+app.post("/login", login);
+app.post("/signup", signUp);
